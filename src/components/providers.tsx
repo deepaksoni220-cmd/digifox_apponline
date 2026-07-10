6a07ee73e85ece65fd2c6acc5a79d47604fdfea5
admin <admin@admins-MacBook-Pro.local>
2026-07-10 17:46:33 +0530

    Revert "Force strict Light Mode only"
    This reverts commit fe041c507ee13d32b8d39c79fc57fcc0ed5db3d5.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -29,7 +29,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" forcedTheme="light" defaultTheme="light" disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
     </QueryClientProvider>
